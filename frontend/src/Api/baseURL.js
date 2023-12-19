5f82a6f83b886c2ed687fbdd0aa31340aa72a982
zezo <egypte.2200@gmail.com>
2023-12-19 04:21:09 +0200

    change baseurl after deploy backend

--- frontend/src/Api/baseURL.js
+++ frontend/src/Api/baseURL.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
-const baseURL = axios.create({baseURL: 'http://localhost:8000'})
+// const baseURL = axios.create({baseURL: 'http://localhost:8000'})
+const baseURL = axios.create({baseURL: 'https://zezo-mern-chat-app-server.onrender.com'})
 
 export default baseURL
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import ChatsPage from "./Pages/ChatsPage/ChatsPage";
 import ProtectedRoute from "./Components/ProtectedRoute";
